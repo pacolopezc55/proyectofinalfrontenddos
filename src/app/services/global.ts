ea72c2539c777b9069418900ff05de4a6ae428a8
pacolopezc55 <pacolopezc55@gmail.com>
2020-05-28 13:05:25 +0200

    ip

--- src/app/services/global.ts
+++ src/app/services/global.ts
@@ -1,4 +1,5 @@
 export var GLOBAL = {
-	url: 'http://localhost:3977/api/',
-	ip: '127.0.0.1'	
+	//url: 'http://localhost:3977/api/',
+	url: 'https://sharefyapi.herokuapp.com/api'
+	//ip: '127.0.0.1'	
 };
